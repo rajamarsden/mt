93810f42581893bb09ba92c5d91effe3a33c1135
rajamarsden <rgpv_rr@hotmail.com>
2021-01-22 22:50:06 +0000

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+## Mocha Test
+
+### Steps to excute the tests
+
+|#|command|
+|--|------|
+|1|`npm install`|
+|1|`npm test`|
---
+++ specs/spec.js
@@ -0,0 +1,21 @@
+import chai from 'chai';
+const { expect, Assert } = chai;
+
+describe('Array Test', () => {
+
+    let names = ["Jackson","Rob","Jack","Wilson","John"]
+
+    it('Assert length of Array', () => {
+      expect(names.length).to.equal(5);
+    })
+
+    it('Assert name contain Daniel', () => {
+       assert.isTrue(names.includes('Daniel'))
+    })
+    it('Assert length of an filtered names', () => {
+      let filterdNames=names.filter(n=>n.startsWith('Jack'))
+      expect(filterdNames.length).to.equal(2);
+    })
+
+
+})
